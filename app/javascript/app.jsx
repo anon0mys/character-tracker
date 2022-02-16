6efb9c86c6361710484d5ff025895d0cda24a5d7
Evan Wheeler <evan.wheeler@onepeloton.com>
2022-02-15 20:01:49 -0700

    Scaffold react and jsbundling + webpack

---
+++ app/javascript/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <div>Hello, Rails 7!</div>
+    )
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rootEl = document.getElementById('app')
+    ReactDOM.render(<App/>, rootEl)
+})
