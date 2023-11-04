e09c1dfa7c1f6ef067425f2f8592bcbb17678d1b
Evan Wheeler <evan.wheeler87@gmail.com>
2023-11-03 19:52:32 -0600

    Add css bundling and css pipeline

--- app/models/concerns/archetypes/archetype.rb
+++ app/models/concerns/archetypes/archetype.rb
@@ -1,4 +1,6 @@
 class Archetypes::Archetype
+  attr_reader :name, :hit_die, :spellcasting_ability
+
   def caster?
     !!@spellcasting_ability
   end
--- app/models/concerns/archetypes/bard.rb
+++ app/models/concerns/archetypes/bard.rb
@@ -1,6 +1,4 @@
 class Archetypes::Bard < Archetypes::Archetype
-  attr_reader :name, :spellcasting_ability
-
   def initialize
     @name = :bard
     @spellcasting_ability = :charisma
